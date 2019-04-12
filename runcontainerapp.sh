0698c9b167648b2721d48a051067264fe198dae7
Ethan Wilansky <ewilansky@yahoo.com>
2019-04-12 15:44:56 -0400

    Added linux/osx runner script

---
+++ runcontainerapp.sh
@@ -0,0 +1,13 @@
+# for NetCore22 app build on Linux/OS X
+
+# cleanup
+rm -R $(pwd)/NetCore22/output
+
+# publish the app to the output directory
+dotnet publish -o $(pwd)/NetCore22/output
+
+docker build -t aspnetcore:v2.2 ./NetCore22/
+
+# run over port 80 for now. Container removed when stopped
+# switch to interactive -it and don't remove (no --rm) when long running
+ docker run --name core --rm -p 80:80 aspnetcore:v2.2
